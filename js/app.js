c3bf44d7d9be23e10f4aff8d955dd442fe7751a5
Dan Drinkard <dan.drinkard@gmail.com>
2012-02-28 01:27:42 -0500

    nav, examples for mobile

--- js/app.js
+++ js/app.js
@@ -2,11 +2,62 @@
   $(function(){
 
     // fancybox
-    $('a.thumb[href$=.png], a.thumb[href$=.jpg], a.thumb[href$=.gif]').fancybox({
-        'titleShow': false
-      , 'transitionIn': 'elastic'
-      , 'transitionOut': 'elastic'
-    });
+    if($(window).width() >= 768){
+      $('a.thumb[href$=.png], a.thumb[href$=.jpg], a.thumb[href$=.gif]').fancybox({
+          'titleShow': false
+        , 'transitionIn': 'elastic'
+        , 'transitionOut': 'elastic'
+      });
+    }
+
+    // scrolling
+    var nav = $('#topbar') // nav element
+      , navY = nav.offset().top // default position
+      , navHeight = nav.height() * 2.5 // nav height for calculating offsets
+      , navLinks = nav.find('a[href^="#"]') // on-page links in nav
+      , pageHeight = $('body').height()
+      , sections = $('article[role=main]').find('section').reverse() // reversed set of nav sections
+      , scrollPos = $(window).scrollTop(); // where are we on the page?
+        $(window).scroll(function(){
+          scrollPos = $(this).scrollTop();
+          // get the active nav item
+          navLinks.removeClass('active');
+          if(scrollPos == 0){
+            $('a[href=#top]').addClass('active');
+          }else{
+            sections.each(function(){
+              if(scrollPos + navHeight >= $(this).offset().top){
+                navLinks.filter('a[href=#' + $(this).attr('id') + ']').addClass('active');
+                return false;
+              }
+            });
+          }
+        });
+        // scrollTo on nav click
+        navLinks.click(function(e){
+          e.preventDefault();
+          var destination = $( $(this).attr('href') ).offset().top,
+            distance;
+          // have we taken the nav out of flow? offset by navHeight if so
+          if(destination > navY){
+            destination -= navHeight;
+          }
+          // distance to scroll as a percent of total height of page
+          distance = Math.abs((scrollPos - destination) / pageHeight);
+          // sqrt the distance to compress the difference a bit
+          // otherwise, short distances will appear to scroll too fast, long ones too slowly
+          // goal is to feel as 'proportional' as possible without actual linearity
+          // this is based on a 1s scroll time for the whole page, adjust as needed
+          $(window).scrollTo(destination, Math.sqrt(distance) * 1000, {easing: 'swing'});
+        });
+
+        //set active state on domready
+        $(window).scroll()
+          // update anything that could be affected by image heights
+          .load(function(){
+            navY = nav.offset().top;
+            pageHeight = $('body').height();
+          });
 
   });
 })(jQuery);
--- js/plugins.js
+++ js/plugins.js
@@ -3,10 +3,10 @@
  * Simple and fancy lightbox alternative
  *
  * Examples and documentation at: http://fancybox.net
- * 
+ *
  * Copyright (c) 2008 - 2010 Janis Skarnelis
  * That said, it is hardly a one-person project. Many people have submitted bugs, code, and offered their advice freely. Their support is greatly appreciated.
- * 
+ *
  * Version: 1.3.4 (11/11/2010)
  * Requires: jQuery v1.3+
  *
@@ -44,3 +44,18 @@ D.append(j=b('<div id="fancybox-content"></div>'),E=b('<a id="fancybox-close"></
 b.fn.mousewheel&&f.bind("mousewheel.fb",function(a,c){if(h)a.preventDefault();else if(b(a.target).get(0).clientHeight==0||b(a.target).get(0).scrollHeight===b(a.target).get(0).clientHeight){a.preventDefault();b.fancybox[c>0?"prev":"next"]()}});b.support.opacity||f.addClass("fancybox-ie");if(M){t.addClass("fancybox-ie6");f.addClass("fancybox-ie6");b('<iframe id="fancybox-hide-sel-frame" src="'+(/^https/i.test(window.location.href||"")?"javascript:void(false)":"about:blank")+'" scrolling="no" border="0" frameborder="0" tabindex="-1"></iframe>').prependTo(D)}}};
 b.fn.fancybox.defaults={padding:10,margin:40,opacity:false,modal:false,cyclic:false,scrolling:"auto",width:560,height:340,autoScale:true,autoDimensions:true,centerOnScroll:false,ajax:{},swf:{wmode:"transparent"},hideOnOverlayClick:true,hideOnContentClick:false,overlayShow:true,overlayOpacity:0.7,overlayColor:"#777",titleShow:true,titlePosition:"float",titleFormat:null,titleFromAlt:false,transitionIn:"fade",transitionOut:"fade",speedIn:300,speedOut:300,changeSpeed:300,changeFade:"fast",easingIn:"swing",
 easingOut:"swing",showCloseButton:true,showNavArrows:true,enableEscapeButton:true,enableKeyboardNav:true,onStart:function(){},onCancel:function(){},onComplete:function(){},onCleanup:function(){},onClosed:function(){},onError:function(){}};b(document).ready(function(){b.fancybox.init()})})(jQuery);
+
+/**
+ * jQuery.ScrollTo - Easy element scrolling using jQuery.
+ * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 5/25/2009
+ * @author Ariel Flesler
+ * @version 1.4.2
+ *
+ * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
+ */
+;(function(d){var k=d.scrollTo=function(a,i,e){d(window).scrollTo(a,i,e)};k.defaults={axis:'xy',duration:parseFloat(d.fn.jquery)>=1.3?0:1};k.window=function(a){return d(window)._scrollable()};d.fn._scrollable=function(){return this.map(function(){var a=this,i=!a.nodeName||d.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!i)return a;var e=(a.contentWindow||a).document||a.ownerDocument||a;return d.browser.safari||e.compatMode=='BackCompat'?e.body:e.documentElement})};d.fn.scrollTo=function(n,j,b){if(typeof j=='object'){b=j;j=0}if(typeof b=='function')b={onAfter:b};if(n=='max')n=9e9;b=d.extend({},k.defaults,b);j=j||b.speed||b.duration;b.queue=b.queue&&b.axis.length>1;if(b.queue)j/=2;b.offset=p(b.offset);b.over=p(b.over);return this._scrollable().each(function(){var q=this,r=d(q),f=n,s,g={},u=r.is('html,body');switch(typeof f){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(f)){f=p(f);break}f=d(f,this);case'object':if(f.is||f.style)s=(f=d(f)).offset()}d.each(b.axis.split(''),function(a,i){var e=i=='x'?'Left':'Top',h=e.toLowerCase(),c='scroll'+e,l=q[c],m=k.max(q,i);if(s){g[c]=s[h]+(u?0:l-r.offset()[h]);if(b.margin){g[c]-=parseInt(f.css('margin'+e))||0;g[c]-=parseInt(f.css('border'+e+'Width'))||0}g[c]+=b.offset[h]||0;if(b.over[h])g[c]+=f[i=='x'?'width':'height']()*b.over[h]}else{var o=f[h];g[c]=o.slice&&o.slice(-1)=='%'?parseFloat(o)/100*m:o}if(/^\d+$/.test(g[c]))g[c]=g[c]<=0?0:Math.min(g[c],m);if(!a&&b.queue){if(l!=g[c])t(b.onAfterFirst);delete g[c]}});t(b.onAfter);function t(a){r.animate(g,j,b.easing,a&&function(){a.call(this,n,b)})}}).end()};k.max=function(a,i){var e=i=='x'?'Width':'Height',h='scroll'+e;if(!d(a).is('html,body'))return a[h]-d(a)[e.toLowerCase()]();var c='client'+e,l=a.ownerDocument.documentElement,m=a.ownerDocument.body;return Math.max(l[h],m[h])-Math.min(l[c],m[c])};function p(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
+
+/* array.reverse */
+$.fn.reverse = [].reverse;
